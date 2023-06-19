1eb8148c02f49534a7372b47d5cf0136aec695f9
William Edmisten <wcedmisten@gmail.com>
2023-06-19 19:40:27 -0400

    Add jupyter notebook

---
+++ README.md
@@ -0,0 +1,15 @@
+# Motivation
+
+A recent proposal for the OpenStreetMap community was to move the 
+official communication channel for discussion data imports from 
+a [mailing list](https://lists.openstreetmap.org/listinfo/imports)
+to the new-ish OSM forum powered by [Discourse](https://www.discourse.org).
+
+As a part of considering this proposal, I'd like to examine how each
+platform is used currently, in order to compare them.
+
+To accomplish this, I scraped both the mailing list archives and the Discourse forum
+for all content going back to the beginning of the year (2023-01-01).
+
+Then, I formatted both datasets and tagged each topic by the relevant country the import
+was discussing (or N/A for general talk).
--- parse_discourse.py
+++ parse_discourse.py
@@ -2,47 +2,44 @@ import requests
 import time
 import json
 
-def scrape_discourse():
+res = requests.get(
+    "https://community.openstreetmap.org/search.json",
+    params={"q": "tags:import after:2023-01-01"},
+    headers={"Accept": "application/json"},
+)
+
+
+all_topics = res.json()
+
+time.sleep(1)
+
+replies = []
+
+for post in all_topics["topics"]:
+    topic_id = post["id"]
+
     res = requests.get(
-        "https://community.openstreetmap.org/search.json",
-        params={"q": "tags:import after:2023-01-01"},
+        f"https://community.openstreetmap.org/t/{topic_id}.json",
         headers={"Accept": "application/json"},
     )
 
+    post_data = res.json()
 
-    all_topics = res.json()
+    # pprint.pprint(post_data)
+
+    for reply in post_data["post_stream"]["posts"]:
+        replies.append(
+            {
+                "username": reply["username"],
+                "date": reply["updated_at"],
+                "subject": reply["topic_slug"],
+                "topic_id": topic_id,
+                "url": f"https://community.openstreetmap.org/t/{topic_id}"
+                # "country": subject_country_map[subject]
+            }
+        )
 
     time.sleep(1)
 
-    replies = []
-
-    for post in all_topics["topics"]:
-        topic_id = post["id"]
-
-        res = requests.get(
-            f"https://community.openstreetmap.org/t/{topic_id}.json",
-            headers={"Accept": "application/json"},
-        )
-
-        post_data = res.json()
-
-        # pprint.pprint(post_data)
-
-        for reply in post_data["post_stream"]["posts"]:
-            replies.append(
-                {
-                    "username": reply["username"],
-                    "date": reply["updated_at"],
-                    "subject": reply["topic_slug"],
-                    "topic_id": topic_id,
-                    "url": f"https://community.openstreetmap.org/t/{topic_id}"
-                    # "country": subject_country_map[subject]
-                }
-            )
-
-        time.sleep(1)
-
-    with open("discourse.json", "w") as f:
-        json.dump(replies, f)
-
-scrape_discourse()
+with open("discourse.json", "w") as f:
+    json.dump(replies, f)
